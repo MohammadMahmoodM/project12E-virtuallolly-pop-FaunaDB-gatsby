cc267737c78a16a8953c7a2b5db2ef52c149d4c2
mahmoodlegend <mohammadmahmood441@gmail.com>
2022-09-07 11:23:16 +0500

    create lolly page form developed

--- src/pages/createNew.js
+++ src/pages/createNew.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../components/Header';
-import {Lolly} from '../components/Lolly';
+import { Lolly } from '../components/Lolly';
 
 export default function createNew() {
     return (
@@ -10,13 +10,32 @@ export default function createNew() {
                 <div>
                     <Lolly fillLollyTop="#d52358" fillLollyMiddle="#e95946" fillLollyBottom="#deaa43"></Lolly>
                 </div>
-                <div>
-                    <input type="color" value="#d52358" className='colorPicker' name="flavourTop" id="flavourTop"/>
-                    <input type="color" value="#e95946" className='colorPicker' name="flavourTop" id="flavourTop"/>
-                    <input type="color" value="#deaa43" className='colorPicker' name="flavourTop" id="flavourTop"/>
+                <div className="lollyFlavourDiv">
+                    <label htmlFor='flavourTop' className="colorPickerLabel">
+                        <input type="color" value="#d52358" className='colorPicker' name="flavourTop" id="flavourTop" />
+                    </label>
+                    <label htmlFor='flavourMiddle' className="colorPickerLabel">
+                        <input type="color" value="#e95946" className='colorPicker' name="flavourMiddle" id="flavourMiddle" />
+                    </label>
+                    <label htmlFor='flavourBottom' className="colorPickerLabel">
+                        <input type="color" value="#deaa43" className='colorPicker' name="flavourBottom" id="flavourBottom" />
+                    </label>
                 </div>
                 <div>
-                    Form
+                    <div className="lollyForm">
+                        <label htmlFor='recipientName'>
+                            To
+                        </label>
+                        <input  type="text" name='recipientName' id='recipientName'/>
+                        <label htmlFor='recipientName'>
+                            Message
+                        </label>
+                        <textarea rows="15" columns="30"/>
+                        <label htmlFor='recipientName'>
+                            From
+                        </label>
+                        <input  type="text" name='recipientName' id='recipientName'/>
+                    </div>
                 </div>
             </div>
         </div>
