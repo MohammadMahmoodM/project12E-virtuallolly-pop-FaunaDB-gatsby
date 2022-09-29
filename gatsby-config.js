aaa407629af2abe8605582056d73c1547d187fe8
mahmoodlegend <mohammadmahmood441@gmail.com>
2022-09-29 16:58:26 +0500

    updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,20 @@
+// In your gatsby-config.js
 module.exports = {
   plugins: [
+    // Simple config, passing URL
     {
       resolve: "gatsby-source-graphql",
       options: {
+        // Arbitrary name for the remote schema Query type
         typeName: "Lolly",
-        fieldName: "Lollies",
-        url: "http://comforting-dragon-48a767.netlify.app/.netlify/functions/newLolly",
+        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
+        fieldName: "lollies",
+        // Url to query from
+        url: "https://comforting-dragon-48a767.netlify.app/.netlify/functions/newLolly",
       },
     },
     {
       resolve: "gatsby-plugin-react-svg",
-      // options: {
-      //   rule: {
-      //     include: /assets/ // See below to configure properly
-      //   }
-      // }
-    },
+    }
   ]
 }
